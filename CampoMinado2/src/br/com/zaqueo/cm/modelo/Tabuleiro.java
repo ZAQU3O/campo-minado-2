84154a0a7814039e04af9d035622793971da2e4a
Zaqueu Soares <bartworld14@gmail.com>
2025-11-10 16:29:07 -0300

    initial commit
    limpeza de codigo

---
+++ CampoMinado2/src/br/com/zaqueo/cm/modelo/Tabuleiro.java
@@ -0,0 +1,85 @@
+package br.com.zaqueo.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Tabuleiro {
+	
+	private int linhas;
+	private int colunas; 
+	private int minas;
+	
+	private final List<Campo> campos = new ArrayList<>();
+	
+	public Tabuleiro() {
+		
+	}
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+		
+		gerarCampos();
+		associarOsVizinhos();
+		sortearMinas();
+	}
+	
+	public void abrir(int linha, int coluna) {
+		try {
+			campos.parallelStream()
+				.filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+				.findFirst()
+				.ifPresent(c -> c.abrir());
+		} catch (Exception e) {
+			//FIXME: IMPLEMENTAR NOVA VERSÃO DA EXCEPTION
+			campos.forEach(c -> c.setAberto(true));
+			throw e;
+		}
+	}
+	
+	public void alternarMarcacao(int linha, int coluna) {
+		campos.parallelStream()
+			.filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+			.findFirst()
+			.ifPresent(c -> c.alternarMarcacao());
+	}
+
+
+	private void gerarCampos() {
+		for(int i = 0; i < linhas; i++) {
+			for (int j = 0; j < colunas; j++) {
+				campos.add(new Campo(i, j));
+			}
+		}
+	}
+	
+	private void associarOsVizinhos() {
+		for(Campo c1: campos) {
+			for(Campo c2: campos) {
+				c1.adicionarVizinho(c2);
+			}
+		}
+	}
+	
+	private void sortearMinas() {
+		long minasArmadas = 0;
+		Predicate<Campo> minado = c -> c.isMinado();
+		
+		do {
+			int aleatorio = (int) (Math.random()*campos.size());
+			campos.get(aleatorio).minar();
+			minasArmadas = campos.stream().filter(minado).count();
+		} while(minasArmadas < minas);
+	}
+	
+	public boolean objetivoAlcancado() {
+		return campos.stream().allMatch(c -> c.objetivoAlcancado());
+	}
+	
+	public void reiniciar() {
+		campos.stream().forEach(c -> c.reiniciar());
+		sortearMinas();
+	}
+}
